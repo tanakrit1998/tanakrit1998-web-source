1140389b08a0bb653b3997fa91ac242b0778d766
tanakrit1998 <tanakrit.ja.59@ubu.ac.th>
2019-10-16 11:12:22 +0700

    add

---
+++ src/app/user.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  friends = [
+    {
+      name: 'Franky Lee Jones',
+      pic: 'frank.png',
+      position: 'CEO',
+    },
+    {
+      name: 'Alice in Wonderland',
+      pic: 'alisa.jpg',
+      position: 'Secretary',
+    },
+    {
+      name: 'Pat Tracy Johnson',
+      pic: 'pat.jpg',
+      position: 'IT Support',
+    }
+  ];
+  users: any
+
+  constructor(private http: HttpClient) {
+    this._getUsers()
+  }
+
+  _getUsers() {
+    return this.http.post(
+      'http://cs.sci.ubu.ac.th:7512/59110440084/user/_search',
+      {
+        query: {
+        
+        }
+      }
+    ); 
+  }
+
+  _saveUser(usr: string, passwd: string) {
+    console.log(`user=${usr} passwd:${passwd}`);
+    return this.http.post(
+      'http://cs.sci.ubu.ac.th:7512/59110440084/user/_create',
+      {
+        user: usr,
+        password: passwd,
+      }
+    );
+  }
+
+  getFriends() {
+    return this.friends;
+  }
+  
+}
